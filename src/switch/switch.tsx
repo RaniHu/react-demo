7aab370bbab1ebbbe83692173556e5122f4a9957
huran <353280368@qq.com>
2017-06-26 11:13:29 +0800

    修改样式

--- src/switch/switch.tsx
+++ src/switch/switch.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDom from 'react-dom';
 
 
-//切换背景色子组件
+//切换子组件背景色
 class SwitchBgChild extends React.Component<any, any> {
 
     defaultProps = {
@@ -13,7 +13,8 @@ class SwitchBgChild extends React.Component<any, any> {
     render() {
 
         return (
-            <div style={{"background":this.props.childBgColor}} className="child">
+            <div className="child">
+                <p>点击改变父组件的背景色</p>
                 <ul>
                     <li><a data-color="#7B77FF" onClick={this.props.changeParentColor}>&nbsp;</a></li>
                     <li><a data-color="#92CCE1" onClick={this.props.changeParentColor}>&nbsp;</a></li>
@@ -26,46 +27,37 @@ class SwitchBgChild extends React.Component<any, any> {
     }
 }
 
-//切换背景色父组件
+//切换父组件背景色
 class SwitchBgParent extends React.Component<any, any> {
 
     state = {
-        parentBgColor: '',
-        tipsColor:'',
-        childBgColor:''
+        parentBgColor: ''
     };
 
     //改变父组件背景色
-    changeParentBgColor(e) {
+    changeParentBgColor(e){
         this.setState({parentBgColor: e.target.getAttribute("data-color")})
-        // let parentTips=ReactDOM.findDOMNode(this.refs.parentTips);
-        this.setState({tipsColor:"#ffffff"});
     };
 
 
     //改变子组件1背景色
-    changeChild1Color (e){
-        ReactDOM.findDOMNode(this.refs.child1).style.background=e.target.getAttribute("data-color");
-        // this.setState({childBgColor:e.target.getAttribute("data-color")})
+    changeChild1Color(e){
+        ReactDom.findDOMNode(this.refs.child1).style.background=e.target.getAttribute("data-color");
     };
 
     //改变子组件2背景色
-    changeChild2Color (e){
-        ReactDOM.findDOMNode(this.refs.child2).style.background=e.target.getAttribute("data-color");
-        // this.setState({childBgColor:e.target.getAttribute("data-color")})
-    
+    changeChild2Color(e){
+        ReactDom.findDOMNode(this.refs.child2).style.background=e.target.getAttribute("data-color");
     };
 
 
     render() {
         let {parentBgColor} = this.state;
-        let {tipsColor} = this.state;
-    
         return (
             <div>
                 <div style={{"background": parentBgColor}} className="parent">
                     <div>
-                        <span style={{"color":tipsColor}}>改变第一个child背景色</span>
+                        <p>点击改变第一个child背景色</p>
                         <ul>
                             <li><a data-color="#7B77FF" onClick={this.changeChild1Color.bind(this)}>&nbsp;</a></li>
                             <li><a data-color="#92CCE1" onClick={this.changeChild1Color.bind(this)}>&nbsp;</a></li>
@@ -75,7 +67,7 @@ class SwitchBgParent extends React.Component<any, any> {
                         </ul>
                     </div>
                     <div>
-                        <span style={{"color":tipsColor}}>改变第二个child背景色</span>
+                        <span>点击改变第二个child背景色</span>
                         <ul>
                             <li><a data-color="#7B77FF" onClick={this.changeChild2Color.bind(this)}>&nbsp;</a></li>
                             <li><a data-color="#92CCE1" onClick={this.changeChild2Color.bind(this)}>&nbsp;</a></li>
@@ -84,11 +76,10 @@ class SwitchBgParent extends React.Component<any, any> {
                             <li><a data-color="#F1684E" onClick={this.changeChild2Color.bind(this)}>&nbsp;</a></li>
                         </ul>
                     </div>
-                <hr/>
                 </div>
-                    <SwitchBgChild ref="child1" changeParentColor={this.changeParentBgColor.bind(this)} childBgColor={this.state.childBgColor}/>
-                    <SwitchBgChild ref="child2" changeParentColor={this.changeParentBgColor.bind(this)} childBgColor={this.state.childBgColor}/>
-
+                <hr/>
+                <SwitchBgChild ref="child1" changeParentColor={this.changeParentBgColor.bind(this)}/>
+                <SwitchBgChild ref="child2" changeParentColor={this.changeParentBgColor.bind(this)}/>
             </div>
         )
     }
